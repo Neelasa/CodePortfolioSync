bff9b3a5778c687c20e782fdd83d2f06134a791f
B. Neelasa <nagadurganeelasabeesu@gmail.com>
2026-08-03 20:40:17 +0530

    Implement LeetCode API client

--- src/leetcode.py
+++ src/leetcode.py
@@ -1,8 +1,39 @@
-"""
-LeetCode Handler
-"""
+import requests
+
 
 class LeetCode:
 
+    def __init__(self, username):
+        self.username = username
+
     def fetch(self):
-        print("Fetching LeetCode submissions...")
+
+        url = "https://leetcode.com/graphql"
+
+        query = """
+        query getUserProfile($username: String!) {
+          matchedUser(username: $username) {
+            username
+            submitStats {
+              acSubmissionNum {
+                difficulty
+                count
+              }
+            }
+          }
+        }
+        """
+
+        variables = {
+            "username": self.username
+        }
+
+        response = requests.post(
+            url,
+            json={
+                "query": query,
+                "variables": variables
+            }
+        )
+
+        print(response.json())
