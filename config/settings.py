6ce19dbab58ba472665861997463a9278611daec
B. Neelasa <nagadurganeelasabeesu@gmail.com>
2026-08-03 20:39:40 +0530

    Update platform configuration
    Added GitHub username and repository settings.

--- config/settings.py
+++ config/settings.py
@@ -1,11 +1,13 @@
-CODECHEF_USERNAME = "u23a81a4304"
+# Usernames
 
 LEETCODE_USERNAME = "Neelasa"
-
+CODECHEF_USERNAME = "u23a81a4304"
 GFG_USERNAME = "nagadurganeelasa"
-
 HACKERRANK_USERNAME = "nagadurganeelas1"
-
 CODE360_USERNAME = "neelasa"
-
 CODOLIO_USERNAME = "Neelasa"
+
+# GitHub
+
+GITHUB_USERNAME = "Neelasa"
+REPOSITORY = "CodePortfolioSync"
